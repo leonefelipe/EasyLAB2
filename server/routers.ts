4aa79c2dbca060ce70a3d122ac1b8021b4382e88
leonefelipe <felipe.leone.venceslau@gmail.com>
2026-04-15 23:28:36 -0300

    Atualizar o routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -6,18 +6,17 @@ import { resumeRouter } from "./resumeRouter";
 import { pdfRouter } from "./pdfRouter";
 import { translateRouter } from "./translateRouter";
 import { jobsRouter } from "./jobsRouter";
+import { linkedinRouter } from "./linkedinRouter";
+import { linkedinParserRouter } from "./linkedinParser";
 
 export const appRouter = router({
-    // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
   system: systemRouter,
   auth: router({
     me: publicProcedure.query(opts => opts.ctx.user),
     logout: publicProcedure.mutation(({ ctx }) => {
       const cookieOptions = getSessionCookieOptions(ctx.req);
       ctx.res.clearCookie(COOKIE_NAME, { ...cookieOptions, maxAge: -1 });
-      return {
-        success: true,
-      } as const;
+      return { success: true } as const;
     }),
   }),
 
@@ -25,13 +24,8 @@ export const appRouter = router({
   pdf: pdfRouter,
   translate: translateRouter,
   jobs: jobsRouter,
-
-  // TODO: add feature routers here, e.g.
-  // todo: router({
-  //   list: protectedProcedure.query(({ ctx }) =>
-  //     db.getUserTodos(ctx.user.id)
-  //   ),
-  // }),
+  linkedin: linkedinRouter,
+  linkedinParser: linkedinParserRouter,
 });
 
 export type AppRouter = typeof appRouter;
